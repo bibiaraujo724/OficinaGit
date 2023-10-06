5784dbd79e7f4c32c26999d38d0463415e7e205c
Gabrielly <bibiaraujo724@gmail.com>
2023-10-06 09:29:06 -0400

    primiro commit

---
+++ GitEGitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.Write("\n Digite um número : ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.Write("\n Digite outro número : ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = num * num2;
+
+        Console.WriteLine($"\n O resultado dessa multiplicação é : {resultado} ");
+
+        Console.ReadKey();
+    }
+}
